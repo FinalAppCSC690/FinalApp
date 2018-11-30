933c2d42153c872e3d6e31b8375e39be716dc24d
Ahmad Hamed Rangeen <ahmadrangeen@sfs-wifi-dhcp-10-143-128-226.wifi.sfsu.edu>
2018-11-29 16:34:52 -0800

    Project created

---
+++ PictureGenerator/PictureGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictureGenerator
+//
+//  Created by Ahmad Hamed Rangeen on 11/29/18.
+//  Copyright © 2018 Ahmad Hamed Rangeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
